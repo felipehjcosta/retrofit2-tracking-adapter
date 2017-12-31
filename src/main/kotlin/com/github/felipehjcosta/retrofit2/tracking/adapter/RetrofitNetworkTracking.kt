96a2543027fef5c17bc4c1d01cf05fe8e4ec45e1
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-31 18:09:03 -0200

    Include tracking path in RetrofitNetworkTracking

--- src/main/kotlin/com/github/felipehjcosta/retrofit2/tracking/adapter/RetrofitNetworkTracking.kt
+++ src/main/kotlin/com/github/felipehjcosta/retrofit2/tracking/adapter/RetrofitNetworkTracking.kt
@@ -3,7 +3,7 @@ package com.github.felipehjcosta.retrofit2.tracking.adapter
 import retrofit2.Response
 
 interface RetrofitNetworkTracking {
-    fun onSuccess(response: Response<*>)
+    fun onSuccess(response: Response<*>, trackingPath: String? = null)
 
-    fun onFailure(throwable: Throwable)
+    fun onFailure(throwable: Throwable, trackingPath: String? = null)
 }
