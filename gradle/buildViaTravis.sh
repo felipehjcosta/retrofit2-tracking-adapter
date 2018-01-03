e007a6ecc104a529cafb110f401b6266c7ffe03c
Felipe Costa <felipe.costa@olxbr.com>
2018-01-03 19:14:43 -0200

    Remove dryRun argument from CI script

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew clean test jacocoJunit5TestReport build bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false --console=plain
+./gradlew clean test jacocoJunit5TestReport build bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" --console=plain
