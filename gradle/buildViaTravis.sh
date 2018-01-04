51a2e6e7a73d6fee1230914c5702264e95a40442
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-01-04 00:42:12 -0200

    Rewrite travis script in order to upload binary to Bintray only when a new tag is created

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./gradlew clean test jacocoJunit5TestReport build bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false --console=plain
+if [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew clean test jacocoJunit5TestReport build bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false --console=plain
+else
+  echo -e 'Normal Build => Branch ['$TRAVIS_BRANCH']'
+  ./gradlew clean test jacocoJunit5TestReport --console=plain
+fi
